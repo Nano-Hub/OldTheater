6fb537e908ce6152a44b45422799b355175f4d6f
NanoHub <stefano.alder@telecom-st-etienne.fr>
2017-01-16 10:22:20 +0100

    add login function to get id (ws)

--- IHM/js/connection.js
+++ IHM/js/connection.js
@@ -1,18 +1,29 @@
- $(document).ready(function()
-   		 	{
-    			$("#buttonSubmit").click(function()
-    			{
-					//alert(document.getElementById("username").value + " " + document.getElementById("password").value);
-					var token = login((document.getElementById("username").value, document.getElementById("password").value));
-					if(token != "")
-					{
-						setCookie("token", token, 1);
-						document.location.href="book.html";
-					}
-					else
-					{
-						alert("Error");
-					}
-    				//$.post( "myFunction", $("#myForm").serialize());
-    			});
-   			});
+function login()
+{
+    var email = document.getElementById("username").value;
+	var psw = document.getElementById("password").value;
+	
+	$.get("	http://localhost:8080/OldTheaterClient/theater/client/login?email="+email+"&password="+psw, function(data)
+	{
+		var response = "";
+		
+		for(var i = 0; i < data.length; i++)
+			{
+			response += data[i];
+			}
+		alert(response + " " response.length);
+		
+		if(data.length == 1)
+		{	
+			setCookie("id", response, 1);	
+			document.location.href="index.html";
+		}
+		else
+		{
+			alert(response); 
+		
+	})
+	.error(function() { 
+		alert("Wrong combination."); 
+	});
+}
