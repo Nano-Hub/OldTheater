405e809d3a0a7625a655935ab6016037ad6ea38a
NanoHub <stefano.alder@telecom-st-etienne.fr>
2017-01-16 13:41:11 +0100

    correction register

--- IHM/js/register.js
+++ IHM/js/register.js
@@ -7,7 +7,8 @@
             
             $.post("http://localhost:8080/OldTheaterClient/theater/client/register?email="+email+"&password="+psw+"&name="+name, function(data)
             {
-
+				document.location.href="eventlist.html";
+				//recuperer id pour cookie
             })
             .error(function() { 
                     alert("You are already in our databases"); 
